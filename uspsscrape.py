02c0fb1d19c42fc03bcdaa38bb7f1ce6151e7236
nlist2 <noah2list@gmail.com>
2020-11-24 17:39:33 -0600

    usps scrape update

--- uspsscrape.py
+++ uspsscrape.py
@@ -7,16 +7,16 @@ import csv
 import pandas as pd
 
 sess = requests.Session()
-usps_link = "https://tools.usps.com/find-location.htm?address=60637&locationType=collectionbox&searchRadius=5&utm_source=google-my-business-url&utm_medium=search&utm_campaign=yext"
-
+usps_lin = "https://tools.usps.com/find-location.htm?address=60637&locationType=collectionbox&searchRadius=5&utm_source=google-my-business-url&utm_medium=search&utm_campaign=yext"
+usps_link = "https://tools.usps.com/find-location.htm?address=60637&locationType=collectionbox"
 specs = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-r = sess.get(str(usps_link), headers = specs)
+r = sess.get(usps_link, headers = specs)
 
 if r.status_code == 200:
     soup = BeautifulSoup(r.content, "html.parser")
-    print(soup.text)
-    result_box = soup.find_all("p", {"class": "address"})
-
+    print(soup.result)
+    result_box = soup.find("div", {"id": "resultBox"})
+    print(result_box.text)
     for result in result_box:
         print(result.text)
 else: 
